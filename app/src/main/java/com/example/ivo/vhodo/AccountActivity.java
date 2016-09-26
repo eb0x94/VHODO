1fb64820b2fc2780b095b1aa8e433f60a4d6edd2
Ivaylo Hristov <megapr0n94@gmail.com>
2016-09-26 16:33:57 +0300

    added buttons to AccountActivity and accent colors.

--- app/src/main/java/com/example/ivo/vhodo/AccountActivity.java
+++ app/src/main/java/com/example/ivo/vhodo/AccountActivity.java
@@ -1,13 +1,42 @@
 package com.example.ivo.vhodo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class AccountActivity extends AppCompatActivity {
+    private boolean isLandlord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        //Checking for admin/landlord account
+        isLandlord = true;//for programming sake
+        if (isLandlord = true) {
+            landlordVisible();
+        }
+    }
+
+    private void landlordVisible() {
+        Button button = (Button) findViewById(R.id.landlord_button);
+        button.setVisibility(Button.VISIBLE);
+    }
+
+    public void onAccountButtonClicked(View view) {
+        switch (view.getId()) {
+            case R.id.debts_button:
+                break;
+            case R.id.payments_button:
+                break;
+            case R.id.balance_button:
+                break;
+            case R.id.logout_button:
+                break;
+            case R.id.landlord_button:
+                break;
+        }
     }
 }
