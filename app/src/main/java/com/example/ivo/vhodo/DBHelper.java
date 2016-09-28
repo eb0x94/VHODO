1a48a5280ab9e8ecd06c85e7d3ef85958eebd753
Vilimir Vladimirov <Viliom1@gmail.com>
2016-09-28 20:42:17 +0300

    Finished the DBHelper class. -Better structure of the class for easier reading -Added methods "Add", "Update" and "Delete" for all tables -Added foreign keys

--- app/src/main/java/com/example/ivo/vhodo/DBHelper.java
+++ app/src/main/java/com/example/ivo/vhodo/DBHelper.java
@@ -11,22 +11,27 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by vilimir on 28.09.16.
  */
 public class DBHelper extends SQLiteOpenHelper {
+
     //user types
     public static final int USER_NORMAL = 0;
     public static final int USER_LANDLORD = 1;
+
     //problem fixed state
     public static final int PROBLEM_FIXED = 0;
     public static final int PROBLEM_IN_PROCESS = 1;
     public static final int PROBLEM_NOT_FIXED = 2;
     public static final int PROBLEM_UNFIXABLE = 3;
+
     //database name
     public static final String DB_NAME = "vhodo_db";
+
     //database tables
     public static final String MECHANIC_CONTACTS_TABLE_NAME = "mechanics";
     public static final String NEIGHBOURS_CONTACTS_TABLE_NAME = "neighbours";
     public static final String MESSAGES_BOARD_TABLE_NAME = "msgboard";
     public static final String USERS_TABLE_NAME = "users";
     public static final String PROBLEMS_TABLE_NAME = "problems";
+
     //database columns
     public static final String ID_COLUMN_NAME = "id";
     public static final String PASS_COLUMN_NAME = "pass";
@@ -39,28 +44,36 @@ public class DBHelper extends SQLiteOpenHelper {
     public static final String ISFIXED_COLUMN_NAME = "isfixed";
     public static final String TYPE_COLUMN_NAME = "type";
 
+    //Constructor
     public DBHelper (Context context){
         super(context,DB_NAME,null,1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(String.format("create table %s (%s integer primary key, %s text,%s text,%s text,%s text)",
+        db.execSQL(String.format("create table %s (%s integer primary key,%s text,%s text,%s text,",
                 NEIGHBOURS_CONTACTS_TABLE_NAME,
-                ID_COLUMN_NAME,NAME_COLUMN_NAME,PHONE_COLUMN_NAME,EMAIL_COLUMN_NAME,USERS_TABLE_NAME));
-        db.execSQL(String.format("create table %s (%s integer primary key, %s text,%s text,%s integer)",
+                ID_COLUMN_NAME,PHONE_COLUMN_NAME,EMAIL_COLUMN_NAME,USERNAME_COLUMN_NAME) +
+                String.format("foreign key(%s) references %s(%s))",
+                        USERNAME_COLUMN_NAME,USERS_TABLE_NAME,USERNAME_COLUMN_NAME)
+        );
+        db.execSQL(String.format("create table %s (%s integer primary key,%s text, %s text,%s text,%s integer)",
                 USERS_TABLE_NAME,
-                ID_COLUMN_NAME,USERNAME_COLUMN_NAME,PASS_COLUMN_NAME,TYPE_COLUMN_NAME));
+                ID_COLUMN_NAME,USERNAME_COLUMN_NAME,PASS_COLUMN_NAME,NAME_COLUMN_NAME,TYPE_COLUMN_NAME));
         db.execSQL(String.format("create table %s (%s integer primary key, %s text,%s text,%s text)",
                 MECHANIC_CONTACTS_TABLE_NAME,
                 ID_COLUMN_NAME,NAME_COLUMN_NAME,PHONE_COLUMN_NAME, TYPE_COLUMN_NAME));
         //TODO need to find a way to get to problem images so it's not going to have images for now.
-        db.execSQL(String.format("create table %s (%s integer primary key, %s text,%s text,%s integer)",
+        db.execSQL(String.format("create table %s (%s integer primary key, %s text,%s text,%s integer,",
                 PROBLEMS_TABLE_NAME,
-                ID_COLUMN_NAME,USERNAME_COLUMN_NAME,PROBLEM_COLUMN_NAME,ISFIXED_COLUMN_NAME));
-        db.execSQL(String.format("create table %s (%s integer primary key, %s text,%s text)",
+                ID_COLUMN_NAME,USERNAME_COLUMN_NAME,PROBLEM_COLUMN_NAME,ISFIXED_COLUMN_NAME) +
+                        String.format("foreign key(%s) references %s(%s))",
+                                USERNAME_COLUMN_NAME,USERS_TABLE_NAME,USERNAME_COLUMN_NAME));
+        db.execSQL(String.format("create table %s (%s integer primary key, %s text,%s text,",
                 MESSAGES_BOARD_TABLE_NAME,
-                ID_COLUMN_NAME,USERNAME_COLUMN_NAME,MESSAGE_COLUMN_NAME));
+                ID_COLUMN_NAME,USERNAME_COLUMN_NAME,MESSAGE_COLUMN_NAME)+
+                String.format("foreign key(%s) references %s(%s))",
+                        USERNAME_COLUMN_NAME,USERS_TABLE_NAME,USERNAME_COLUMN_NAME));
 
     }
 
@@ -69,22 +82,40 @@ public class DBHelper extends SQLiteOpenHelper {
 
     }
 
-    public boolean addNewUser(int id,String username, String pass,int type){
+    //User table methods
+    public boolean addNewUser(int id,String username, String pass,String name,int type){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
         cv.put(ID_COLUMN_NAME,id);
         cv.put(USERNAME_COLUMN_NAME,username);
         cv.put(PASS_COLUMN_NAME,pass);
+        cv.put(NAME_COLUMN_NAME,name);
         cv.put(TYPE_COLUMN_NAME,type);
         db.insert(USERS_TABLE_NAME,null,cv);
         return true;
     }
 
-    public boolean addNewNeighbour(int id, String name, String phone, String email, String username){
+    public boolean updateUser(int id, String username,String pass,String name, int type){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(USERNAME_COLUMN_NAME,username);
+        cv.put(PASS_COLUMN_NAME,pass);
+        cv.put(NAME_COLUMN_NAME,name);
+        cv.put(TYPE_COLUMN_NAME,type);
+        db.update(USERS_TABLE_NAME,cv,ID_COLUMN_NAME + " = ?",new String[]{Integer.toString(id)});
+        return true;
+    }
+    public boolean deleteUser(int id){
+        SQLiteDatabase db = getWritableDatabase();
+        db.delete(USERS_TABLE_NAME,ID_COLUMN_NAME + " = ?",new String[]{Integer.toString(id)});
+        return true;
+    }
+
+    //Neighbour table methods
+    public boolean addNewNeighbour(int id, String phone, String email, String username){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
         cv.put(ID_COLUMN_NAME,id);
-        cv.put(NAME_COLUMN_NAME,name);
         cv.put(PHONE_COLUMN_NAME,phone);
         cv.put(EMAIL_COLUMN_NAME,email);
         cv.put(USERNAME_COLUMN_NAME,username);
@@ -92,6 +123,29 @@ public class DBHelper extends SQLiteOpenHelper {
         return true;
     }
 
+    public boolean updateNeigbour(int id, String phone, String email, String username){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(PHONE_COLUMN_NAME,phone);
+        cv.put(EMAIL_COLUMN_NAME,email);
+        cv.put(USERNAME_COLUMN_NAME,username);
+        db.update(NEIGHBOURS_CONTACTS_TABLE_NAME,cv,ID_COLUMN_NAME + " = ?",new String[]{Integer.toString(id)});
+        return true;
+    }
+
+    public boolean deleteNeigbour(int id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(NEIGHBOURS_CONTACTS_TABLE_NAME,ID_COLUMN_NAME + " = ?", new String[]{Integer.toString(id)});
+        return true;
+    }
+
+    public boolean deleteNeighbour(String username){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(NEIGHBOURS_CONTACTS_TABLE_NAME,USERNAME_COLUMN_NAME + " ?",new String[]{username});
+        return true;
+    }
+
+    //Problem table methods
     public boolean addNewProblem(int id, String user, String problem, int state){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
@@ -102,6 +156,19 @@ public class DBHelper extends SQLiteOpenHelper {
         db.insert(PROBLEMS_TABLE_NAME,null,cv);
         return true;
     }
+
+    public boolean updateProblem(int id, String user, String problem, int state){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(USERNAME_COLUMN_NAME,user);
+        cv.put(PROBLEM_COLUMN_NAME,problem);
+        cv.put(ISFIXED_COLUMN_NAME,state);
+        db.update(PROBLEMS_TABLE_NAME,cv,ID_COLUMN_NAME + " = ?", new String[]{Integer.toString(id)});
+        return true;
+    }
+
+
+    //Mechanics table methods
     public boolean addNewMechanic(int id, String name, String phone, String type){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues() ;
@@ -112,9 +179,32 @@ public class DBHelper extends SQLiteOpenHelper {
         db.insert(MECHANIC_CONTACTS_TABLE_NAME,null,cv);
         return true;
     }
-    public Cursor getData(int id){
+
+    public boolean updateMechanic(int id, String name, String phone, String type){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(NAME_COLUMN_NAME,name);
+        cv.put(PHONE_COLUMN_NAME,phone);
+        cv.put(TYPE_COLUMN_NAME,type);
+        db.update(MECHANIC_CONTACTS_TABLE_NAME,cv,ID_COLUMN_NAME + " = ?" ,new String[]{Integer.toString(id)});
+        return true;
+    }
+
+    public boolean deleteMechanic(int id){
+        SQLiteDatabase db = getWritableDatabase();
+        db.delete(MECHANIC_CONTACTS_TABLE_NAME,ID_COLUMN_NAME + " = ?" , new String[]{Integer.toString(id)});
+        return true;
+    }
+    public boolean deleteMechanic(String name){
+        SQLiteDatabase db = getWritableDatabase();
+        db.delete(MECHANIC_CONTACTS_TABLE_NAME,ID_COLUMN_NAME + " = ?", new String[]{name});
+        return true;
+    }
+
+    //additional public methods
+    public Cursor getDataWithId(String table,int id){
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res =  db.rawQuery( "select * from contacts where id="+id+"", null );
+        Cursor res =  db.rawQuery( String.format("select * from %s where id = %d",table,id), null );
         return res;
     }
 
@@ -124,4 +214,6 @@ public class DBHelper extends SQLiteOpenHelper {
         return numRows;
     }
 
+    //private methods5
 }
+
